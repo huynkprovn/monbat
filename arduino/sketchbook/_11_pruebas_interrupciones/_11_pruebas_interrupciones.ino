dd4144b4475c570f0942dc3c07015017bdf2bb4c
antonio1010.mr@gmail.com <antonio1010.mr@gmail.com@0e5eec27-93d2-06cb-cb2e-47110a511d83>
2012-11-01 22:19:59 +0000

    Se desactiva la interrupcion al inicio de la rutina de tratamiento y se activa al final. Corregido un error en el codigo (falta sentencia break). Añadida funcion antirrebote sw. No se producen rebotes pero sigue entrando dos veces exporadicamente en la parte de la rutina que trata la llegada de señal LOW

--- arduino/sketchbook/_11_pruebas_interrupciones/_11_pruebas_interrupciones.ino
+++ arduino/sketchbook/_11_pruebas_interrupciones/_11_pruebas_interrupciones.ino
@@ -11,40 +11,67 @@
 const int GreenLED = 12;
 const int RedLED = 11;
 const int InterruptPin = 2;
-
+const int debounceDelay = 10;
 void setup()
 {
   pinMode(GreenLED, OUTPUT);
   pinMode(RedLED, OUTPUT);
   pinMode(InterruptPin, INPUT);
   digitalWrite(InterruptPin,HIGH);
-  attachInterrupt(0,interrupHandler,CHANGE);
+  attachInterrupt(0,interrupHandler,LOW);
   Serial.begin(9600);
 }
 
 void interrupHandler()
 {
-  switch(digitalRead(InterruptPin))
+  detachInterrupt(0);
+  switch(debounce(InterruptPin))
   {
   case HIGH:
-    attachInterrupt(0,interrupHandler,LOW);
+    //detachInterrupt(0);
     digitalWrite(GreenLED,HIGH);
     delay(1000);
     digitalWrite(GreenLED,LOW);
     //Serial.println("High level interrupt detection");
     Serial.println("1");
     attachInterrupt(0,interrupHandler,LOW);
+    break;
+    //attachInterrupt(0,interrupHandler,LOW);
   case LOW:
-    
+    //detachInterrupt(0);
     digitalWrite(RedLED,HIGH);
     delay(1000);
     digitalWrite(RedLED,LOW);
     //Serial.println("Low level interrupt detection");
     Serial.println("0");
     attachInterrupt(0,interrupHandler,HIGH);
+    break;
   }
     
 }
+
+boolean debounce(int pin)
+{
+  boolean state;
+  boolean previousState;
+  
+  Serial.print("i");
+  previousState = digitalRead(pin); //store pin state
+  for (int counter=0; counter < debounceDelay; counter++)
+  {
+    delay(1);  // wait for 1 milisecond
+    state = digitalRead(pin);
+    if (state != previousState)
+    {
+      counter = 0; // reset the counter if the state changes
+      previousState = state;
+    }
+  }
+  // here when the switch state has been stable longer than debounce time
+  Serial.print("f");
+  return state;
+}
+
 
 void loop()
 {
