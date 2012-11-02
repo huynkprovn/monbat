2523f99945230d8e41ea007ba84bd2b2de4bda61
antonio1010.mr@gmail.com <antonio1010.mr@gmail.com@0e5eec27-93d2-06cb-cb2e-47110a511d83>
2012-11-02 18:44:30 +0000

    add call to sleep functions. tofix: fail when entering sleep mode. fail sometimes when receiving interrupts

--- arduino/sketchbook/_12_pruebas_sleep_mode/_12_pruebas_sleep_mode.ino
+++ arduino/sketchbook/_12_pruebas_sleep_mode/_12_pruebas_sleep_mode.ino
@@ -38,6 +38,7 @@ void pin2int()
 {
   detachInterrupt(0);
   Serial.println("2");
+  power_all_enable();
   attachInterrupt(0,pin2int,FALLING);
 }
 
@@ -46,6 +47,45 @@ void pin3int()
 {
   detachInterrupt(1);
   Serial.println("3");
+  /* Now is the time to set the sleep mode. In the Atmega8 datasheet
+     * http://www.atmel.com/dyn/resources/prod_documents/doc2486.pdf on page 35
+     * there is a list of sleep modes which explains which clocks and 
+     * wake up sources are available in which sleep modus.
+     *
+     * In the avr/sleep.h file, the call names of these sleep modus are to be found:
+     *
+     * The 5 different modes are:
+     *     SLEEP_MODE_IDLE         -the least power savings 
+     *     SLEEP_MODE_ADC
+     *     SLEEP_MODE_PWR_SAVE
+     *     SLEEP_MODE_STANDBY
+     *     SLEEP_MODE_PWR_DOWN     -the most power savings
+     *
+     *  the power reduction management <avr/power.h>  is described in 
+     *  http://www.nongnu.org/avr-libc/user-manual/group__avr__power.html
+       
+  */   
+  set_sleep_mode(SLEEP_MODE_IDLE);   // sleep mode is set here
+
+  sleep_enable();          // enables the sleep bit in the mcucr register
+                             // so sleep is possible. just a safety pin 
+  
+  //power_adc_disable();
+  //power_spi_disable();
+  //power_timer0_disable();
+  //power_timer1_disable();
+  //power_timer2_disable();
+  //power_twi_disable();
+  
+  
+  sleep_mode();            // here the device is actually put to sleep!!
+ 
+                            // THE PROGRAM CONTINUES FROM HERE AFTER WAKING UP
+  sleep_disable();         // first thing after waking from sleep:
+                            // disable sleep...
+
+  
+
   attachInterrupt(1,pin3int,FALLING);
 }
 
@@ -56,16 +96,18 @@ void serialEvent()
   if (ch == 'D') {
     Serial << hour() << ":" << minute() << ":" << second();
     Serial.println();
-    //blinking(RedLED,2,100);
+    blinking(RedLED,2,100);
   }
 }
+
 
 void sendtime()
 {
   Serial << year() << "/" << month() << "/" << day()<< " " << hour() << ":" << minute() << ":" << second();
   Serial.println();
-  //blinking(GreenLED,5,100);
+  blinking(GreenLED,5,100);
 }
+
 
 void blinking(int pin, int times, int period)
 {
@@ -77,6 +119,7 @@ void blinking(int pin, int times, int period)
     delay(period);
   }  
 }
+
 
 void loop()
 {  
